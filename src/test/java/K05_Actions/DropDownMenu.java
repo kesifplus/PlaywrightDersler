2f68fcda0defd079610a3f41f286445f86fd844e
gokhan agar <gokhanagar1@gmail.com>
2024-07-27 22:32:59 +0300

    V2

---
+++ src/test/java/K05_Actions/DropDownMenu.java
@@ -0,0 +1,54 @@
+package K05_Actions;
+
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.SelectOption;
+
+import java.awt.*;
+
+public class DropDownMenu {
+    public static void main(String[] args) throws InterruptedException {
+
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = (int) dimension.getWidth();
+        int height = (int) dimension.getHeight();
+
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+
+        Page page = browser.newPage();
+        page.setViewportSize(width, height);
+
+        page.navigate("https://www.ebay.com/");
+
+        // Select options
+        Locator selectCategory = page.getByLabel("Select a category for search");
+        Thread.sleep(3000);
+
+        // select by value
+        selectCategory.selectOption("2984");
+        Thread.sleep(3000);
+
+        // select by label
+        selectCategory.selectOption(new SelectOption().setLabel("Consumer Electronics"));
+        Thread.sleep(3000);
+
+        page.close();
+        browser.close();
+        playwright.close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ src/test/java/K05_Actions/TextInputKeysAndShortcuts.java
@@ -0,0 +1,58 @@
+package K05_Actions;
+
+import com.microsoft.playwright.*;
+
+import java.awt.*;
+
+public class TextInputKeysAndShortcuts {
+    public static void main(String[] args) throws InterruptedException {
+
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = (int) dimension.getWidth();
+        int height = (int) dimension.getHeight();
+
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+
+        Page page = browser.newPage();
+        page.setViewportSize(width, height);
+
+        page.navigate("https://www.ebay.com/");
+
+        // Text input
+        Locator searchBox = page.getByPlaceholder("Search for anything");
+        searchBox.fill("bicycle");
+
+        // Keys and shortcuts
+        //searchBox.press("Enter");
+        page.keyboard().press("Enter");
+
+        Thread.sleep(3000);
+
+
+        page.close();
+        browser.close();
+        playwright.close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
